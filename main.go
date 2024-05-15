ff0fa9896cd69352d6965ee5d94c17bb5d1ff72a
Bergomezz <bernardogomezz@yahoo.com.br>
2024-05-15 12:34:42 -0300

    feature: deck part

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello, world!")
+	cards := deck{"Ace of Dimonds", newCard()}
+	cards = append(cards, "Six of Diamonds")
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
